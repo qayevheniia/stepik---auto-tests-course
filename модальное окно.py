27df5924139ef98efb5f0a3a76611a376e251ea7
qayevheniia <qayevheniia@gmail.com>
2020-05-18 17:43:23 +0300

    Проверка текста на соответствие конкретному тексту

---
+++ модальное окно.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+     return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector("button.btn.btn-primary").click()
+    confirm = browser.switch_to.alert #модальное окна
+    confirm.accept() #согласиться с модальном окном
+
+    formula = browser.find_element_by_id("input_value")
+    x = formula.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    option1 = browser.find_element_by_css_selector("button.btn.btn-primary")
+    option1.click()
+
+
+
+finally:
+    time.sleep(10)
+    browser.quit()
+
